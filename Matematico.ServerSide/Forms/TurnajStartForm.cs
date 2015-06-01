9024a2b7f09c86c7acc759a087d5cad5f30496bd
David Blažek <david.blazek@ambico.cz>
2015-06-02 00:28:15 +0200

    Create TurnajStartForm.cs

---
+++ Matematico.ServerSide/Forms/TurnajStartForm.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace matematico_server
+{
+    public partial class TurnajStartForm : Form
+    {
+        public TurnajStartForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int pocet = int.Parse(textBox1.Text);
+        }
+    }
+}
