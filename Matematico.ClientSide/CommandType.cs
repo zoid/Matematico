d3ace2ed09ef85db4d168b5bc3f2646123f9fa04
zoid <david.blazek@ambico.cz>
2015-06-01 23:38:55 +0200

    Create CommandType.cs

---
+++ Matematico.ClientSide/CommandType.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matematico.ClientSide
+{
+    public enum CommandType
+    {
+        NaC,
+        Start,
+        Numbers,
+        Timelimit
+    }
+}
